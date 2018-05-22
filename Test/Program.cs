300883fe7c167eb8396b260dd141d5d2bbb3d8a1
Romain Bancken <rbford@proximus.be>
2018-05-22 09:20:25 +0200

    aanpassing 1

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,6 +10,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Dit is een test voor Git");
+            Console.WriteLine("Aanpassing 1");
         }
     }
 }
